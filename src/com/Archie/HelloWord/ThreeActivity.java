058b44b52da43c957854596bb38cf55d9687715e
inconceivable-man <inconceivable-man@163.com>
2015-10-15 20:17:49 +0800

    添加

---
+++ src/com/Archie/HelloWord/ThreeActivity.java
@@ -0,0 +1,17 @@
+package com.Archie.HelloWord;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created
+ * Author: yuqi
+ * Email：inconceivable_man@163.com
+ * Date: 2015/10/15
+ */
+public class ThreeActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
